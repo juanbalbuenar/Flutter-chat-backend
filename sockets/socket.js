c5ba74553d2dc8becf645758aeae2f484fea3afb
juanbalbuenar <urisoftteam@gmail.com>
2020-09-12 18:23:00 -0500

    Fin del backend

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,36 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const {usuarioConectado, usuarioDesconectado, grabarMensaje} = require('../controllers/socket');
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token'] );
+    console.log(valido, uid);
+
+    // Verificar autenticacion
+    if(!valido) {return client.disconnect(); }
+
+    // Cliente autenticado
+    usuarioConectado(uid);
+
+    // Ingresar al usuario a una sala en particular
+    // sala global, client.id
+    client.join(uid);
+
+    // Escuchar del cliente el mensaje-personal
+    client.on('mensaje-personal', async (payload) => {
+        // Grabar mensaje
+        await grabarMensaje(payload)
+        io.to(payload.para).emit('mensaje-personal', payload);
+    })
+
+
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
   
